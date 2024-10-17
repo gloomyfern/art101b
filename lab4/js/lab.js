a9f942c9dc2951ec2fe2d6ca05dca97d136e81e8
Fia <122323341+gloomyfern@users.noreply.github.com>
2024-10-17 16:36:12 -0700

    Update lab.js

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Fia McCord
+// Date: October 2024
 
 // Constants
 
@@ -17,5 +17,26 @@ function main() {
   // the code that makes everything happen
 }
 
-// let's get this party started
+// Rock, Paper, Scissors
+//An implementation of a game
+//Fia McCord
+//Ocotober 2024
+
+  //prompt user to enter rock, paper, or scissors
+  //accept user input of rock, paper, or scissors
+  //computer randomly choose rock, paper, or scissors
+  //test of win or draw condition
+    //User rock + computer paper = computer win
+    //User rock + computer scissors = user win
+    //user paper + computer scissors = computer win
+    //User paper + computer rock = user win
+    //User scissors + computer rock = computer win
+    //User scissors + computer paper = user win
+    //User rock + computer rock = draw
+    //User paper + computer paper = draw
+    //User scissors + computer scissors = draw
+
+
+
+
 main();
