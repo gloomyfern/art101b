617d3ffe78f9c836f4311d3f1ad755eb3369b096
Fia <122323341+gloomyfern@users.noreply.github.com>
2024-10-21 16:11:31 -0700

    Create lab5.js

---
+++ lab5/js/lab5.js
@@ -0,0 +1,23 @@
+/*
+ * Author:    Fia McCord 
+ * Created:   20 October
+ * License: Public Domain
+ */
+
+// Define Variables
+make = "Toyota";
+model = "Corola";
+color = "Silver";
+year = 2012;
+
+// calculate
+age = 2024 - year;
+
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+
