cd3258315ea492665669c4a5c61731dc0448266d
Fia <122323341+gloomyfern@users.noreply.github.com>
2024-10-29 16:03:31 -0700

    Update lab8.js

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -2,28 +2,23 @@
 // Author: Fia McCord
 // Date: October 2024
 
-function EvenNumbers(x){
-  var results = 5
-  return results;
+function isEven(x){
+  return(x % 2 == 0)
 }
 
+//test function
+console.log("Is 1 even?", isEven(1));
+console.log("Is 2 even?", isEven(2));
 
-array.map(yourFunctionName);
+array = [100, 81, 4, 16, 42, 144, 10000]
+console.log("My array", array);
 
+var result = array.map(isEven);
+// should return [true, false, true, true, true, true, true]
+console.log("Test of evens in the array", result);
 
-array.map(function(x){
-  var results = 7
-  return results;
+var result = array.map(function(x){
+  return x **0.5;
 })
-
-
-var mapResults = array.map(yourFunctionName);
-console.log("Results: ", mapResults) 
-var numbers = [1, 2, 3, 4, 5, 6];
-
-var numbers = [1, 2, 3, 4, 5, 6];
-var evenNumbers = numbers.filter(function(num) {
-    return num % 2 === 0;
-});
-
-console.log(evenNumbers);
+// should return [10, 9, 2, 4, 6.48074069840786, 12, 100]
+console.log("Squareroot of array:", result);
