1463719e5a9ca4ae53f41473c639b5de581a4067
Fia <122323341+gloomyfern@users.noreply.github.com>
2024-10-29 15:38:39 -0700

    Create lab8.js

---
+++ lab8/js/lab8.js
@@ -0,0 +1,29 @@
+// index.js - Lab 8
+// Author: Fia McCord
+// Date: October 2024
+
+function EvenNumbers(x){
+  var results = 5
+  return results;
+}
+
+
+array.map(yourFunctionName);
+
+
+array.map(function(x){
+  var results = 7
+  return results;
+})
+
+
+var mapResults = array.map(yourFunctionName);
+console.log("Results: ", mapResults) 
+var numbers = [1, 2, 3, 4, 5, 6];
+
+var numbers = [1, 2, 3, 4, 5, 6];
+var evenNumbers = numbers.filter(function(num) {
+    return num % 2 === 0;
+});
+
+console.log(evenNumbers);
