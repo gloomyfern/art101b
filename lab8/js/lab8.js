b227bc2734e84d2b5ebb276d8d7631bb38b7ca8f
Fia <122323341+gloomyfern@users.noreply.github.com>
2024-10-30 16:49:43 -0700

    Update lab8.js

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -2,23 +2,13 @@
 // Author: Fia McCord
 // Date: October 2024
 
-function isEven(x){
-  return(x % 2 == 0)
+
+function chooseColor(){
+  let askColor = window.prompt("What color should this page be?");
+  console.log (askColor);
+  return askColor;
 }
 
-//test function
-console.log("Is 1 even?", isEven(1));
-console.log("Is 2 even?", isEven(2));
 
-array = [100, 81, 4, 16, 42, 144, 10000]
-console.log("My array", array);
+$("BODY").css("background-color", chooseColor());
 
-var result = array.map(isEven);
-// should return [true, false, true, true, true, true, true]
-console.log("Test of evens in the array", result);
-
-var result = array.map(function(x){
-  return x **0.5;
-})
-// should return [10, 9, 2, 4, 6.48074069840786, 12, 100]
-console.log("Squareroot of array:", result);
