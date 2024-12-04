73ec09a99473003b4127ae49729e4b43d86de99b
Fia <122323341+gloomyfern@users.noreply.github.com>
2024-12-03 17:46:30 -0800

    Create lab14.js

---
+++ lab14/js/lab14.js
@@ -0,0 +1,15 @@
+/*
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: Fia McCord
+   Date: November 2024
+*/
+
+
+//Loop through numbers 1 to 200, listing them as you go
+//If the number is a multiple of 3, it should print "Fizzy"
+//If the number is a multiple of 5, it should print "Buzzy"
+//If the number is a multiple of 7, it should print "Boom"
+//If the number is a multiple of more than 1, 3, 5, or 7 it should combine Fizzy, Buzzy, and Boom.
+
